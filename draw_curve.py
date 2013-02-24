a0bda51cac0a03a25d94696520b9c55d53b09721
Yoann Ricordel <leyaude@gmail.com>
2013-02-24 12:03:45 +0100

    Adding the python plotter

---
+++ draw_curve.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plot
+import numpy
+
+
+def get_values(filename):
+    # There should be only one line in each file
+    line = open(filename).readlines()[0].strip()
+    vals = [float(v) for v in line.split(" ")]
+    return vals
+
+def print_vals(ys):
+    xs = numpy.linspace(0, 1.0, 1002)
+    plot.plot(xs, ys)
+    plot.show()
+
+
+
+if __name__ == "__main__":
+    import sys
+    import itertools
+    # Get all the files IN ORDER, concatenate their content,
+    # and draw the result.
+    if len(sys.argv) != 3:
+        print("Usage: %s <file prefix> <number of files>" % sys.argv[0])
+    file_prefix = sys.argv[1]
+    nb_files = int(sys.argv[2])
+
+    files = ["%s%i.dat" % (file_prefix, i) for i in range(nb_files)]
+    vals = [get_values(f) for f in files]
+    vals = list(itertools.chain(*vals))
+    vals = [0] + vals + [0]
+
+    print_vals(vals)
